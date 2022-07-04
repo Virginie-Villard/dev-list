57a88a2bcb25f53a9b87475cb7c6605b1bb965a3
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-07-04 11:59:34 +0200

    Create Index.vue & display list of devs

---
+++ src/components/Index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+
+        <div class="dev" v-for="currency in response" :key="currency.email">
+
+            <img :src="currency.picture.medium" :alt="currency.name.last+' picture'">
+
+            <div class="name">
+            {{ currency.name.title }} {{ currency.name.first }} {{ currency.name.last }}
+            </div>
+
+            <div class="email">
+                {{ currency.email }}
+            </div>
+
+            <div class="phone">
+                {{ currency.phone }}
+            </div>
+
+        </div>
+
+        <pre>
+            {{ response }}
+        </pre>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: 'Index',
+
+    props: {
+        msg: String
+    },
+
+    data () {
+        return {
+            response: null
+        }
+    },
+
+    mounted() {
+        document.title = "Developers Index";
+
+        axios
+            .get('https://randomuser.me/api/')
+            .then(response => (this.response = response.data.results))
+    }
+}
+</script>
+
+<style>
+
+</style>
