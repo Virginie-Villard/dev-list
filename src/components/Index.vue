af0be4ddc8040b7bad5ac655a7d75e7fe7bc1000
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-07-04 13:53:39 +0200

    Valid devs list in Index.vue

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,27 +2,32 @@
     <div>
         <h1>{{ msg }}</h1>
 
-        <div class="dev" v-for="currency in response" :key="currency.email">
+        <div class="devList">
 
-            <img :src="currency.picture.medium" :alt="currency.name.last+' picture'">
+            <div class="devCard" v-for="currency in response" :key="currency.email">
+                <img :src="currency.picture.medium" :alt="currency.name.last+' picture'">
 
-            <div class="name">
-            {{ currency.name.title }} {{ currency.name.first }} {{ currency.name.last }}
+                <h2 class="name">
+                {{ currency.name.title }} {{ currency.name.first }} {{ currency.name.last }}
+                </h2>
+
+                <div class="email">
+                    <strong>e-mail: </strong>{{ currency.email }}
+                </div>
+
+                <div class="phone">
+                    <strong>Phone: </strong>{{ currency.phone }}
+                </div>
+
+                <input class="button" type="button" @click="displayCard" :value="'See ' + currency.name.last">
             </div>
 
-            <div class="email">
-                {{ currency.email }}
-            </div>
-
-            <div class="phone">
-                {{ currency.phone }}
-            </div>
 
         </div>
 
-        <pre>
-            {{ response }}
-        </pre>
+<!--        <pre>-->
+<!--            {{ response }}-->
+<!--        </pre>-->
     </div>
 </template>
 
@@ -46,12 +51,38 @@ export default {
         document.title = "Developers Index";
 
         axios
-            .get('https://randomuser.me/api/')
+            .get('https://randomuser.me/api/?results=12')
             .then(response => (this.response = response.data.results))
     }
 }
 </script>
 
 <style>
+body {
+    margin: 0 200px;
+}
+
+h1, h2 {
+    color: darkblue;
+}
+
+.name, .email {
+    margin: 20px;
+}
+
+.devList {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+}
+
+.devCard {
+    flex: 0 0 26%;
+    margin: 20px auto;
+    border-radius: 6px;
+    box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.40);
+    height: 300px; /* A changer ! */
+    padding: 20px;
+}
 
 </style>
